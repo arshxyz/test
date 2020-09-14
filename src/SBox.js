d0ac6b3b7253ca5423a9c7280fcbf1d976547052
arsh <test@test.com>
2020-09-14 14:07:29 +0530

    proper search implementation

--- src/SBox.js
+++ src/SBox.js
@@ -1,17 +1,23 @@
 import React, {useState, useRef} from 'react';
 import _ from 'lodash';
 import 'semantic-ui-css/semantic.min.css';
-import {Search} from 'semantic-ui-react';
+import {Search, Grid} from 'semantic-ui-react';
 import data from './my.json';
 
 function SBox(props){
 
     const [searchdata, setSearchdata] = useState("");
-    const [isLoading, setLoading] = useState(false)
+    const [isLoading, setLoading] = useState(false);
+    const [results, setResults] = useState([{"title": "test"}]);
     const cval = useRef(_.debounce((val) => {
-        document.title = val;
-        setLoading(false);  
-    }, 800)).current
+        document.title = val ? "Search - " + val : "";
+        setLoading(false);
+        const re = new RegExp(_.escapeRegExp(val), 'i');
+        const isMatch = (result) => re.test(result.replace('-', ' '));
+        const res = _.filter(data, (item) => isMatch(item.name)).map((item) => {return {'title': item.model, 'price': "₹" + item.price}});
+        setResults(res);
+
+    }, 500)).current
 
 
     const handleSearch = (e, {value}) => {
@@ -21,7 +27,13 @@ function SBox(props){
     }
 
 
-    return <Search value={searchdata} onSearchChange={handleSearch} loading={isLoading} align={'center'}/>
+    return <Grid centered> <Grid.Column largeScreen={3} mobile={12} textAlign="center"> 
+            <Search fluid
+                value={searchdata} 
+                onSearchChange={handleSearch} 
+                loading={isLoading}  
+                results={results}
+            /> </Grid.Column>  </Grid>
 }
 
 export default SBox;
